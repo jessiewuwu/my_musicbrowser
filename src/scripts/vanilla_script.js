e10dc6d8b11fc6d247bade2745c9e686e17d0bd2
jessiewuwu <jwu004@gmail.com>
2016-07-13 20:26:16 -0700

    Add script and button for Top Songs to show top 3 and then all songs with onclick event

--- src/scripts/vanilla_script.js
+++ src/scripts/vanilla_script.js
@@ -1,12 +1,17 @@
 function toggleResponsiveNav(){
-  var mobileNavIcon = document.getElementById('hamburger-menu');
-  mobileNavIcon.onclick = renderMenu;
+
+  // toggle mobile nav bar
+  document.getElementById('hamburger-menu').onclick = renderMenu;
   document.getElementById('exit-icon').onclick = closeMenu;
+
+  // see more/less Top Songs mobile
+  document.getElementById('display-toggle').onclick = toggleSongs;
 }
 
 function renderMenu() {
   var responsiveMenu = document.getElementById('responsive-nav');
   responsiveMenu.style.display = 'block';
+  
   document.getElementById('hamburger-menu').style.display = 'none';
   document.getElementById('search-button').style.display = 'none';
   document.getElementById('exit-icon').style.display = 'block';
@@ -17,4 +22,19 @@ function closeMenu(){
   document.getElementById('hamburger-menu').style.display = 'block';
   document.getElementById('search-button').style.display = 'block';
   document.getElementById('exit-icon').style.display = 'none';
+}
+
+function toggleSongs() {
+  var i,
+  lastThreeSongs = document.querySelectorAll('.right-section li:nth-last-child(-n+3)')
+  
+  for(i = 0; i < lastThreeSongs.length; i++){
+    if(lastThreeSongs[i].style.display == '') {
+      lastThreeSongs[i].style.display = 'block';
+      this.innerHTML = 'See Less';
+    } else {
+      lastThreeSongs[i].style.display = '';
+      this.innerHTML = 'See More';
+    }
+  }
 }
